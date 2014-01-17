af83ea632dac48daea623d296db5f849252eea18
王沃伦 <just_woolen@qq.com>
2014-01-17 09:51:41 +0800

    Install

--- install.bat
+++ install.bat
@@ -1,6 +1,14 @@
-@echo off
-echo "woolenæ­£åœ¨åŠªåŠ›ä¸ºæ‚¨å®‰è£…bundleç¨‹åº" > woolen
-echo "å®‰è£…å®Œæ¯•å°†è‡ªåŠ¨é€€å‡º" >> woolen
-echo "è¯·è€å¿ƒç­‰å¾…" >> woolen
+@echo on
+move /y %USERPROFILE%\vim %USERPROFILE%\vim_old 
+cd %USERPROFILE%
+c:
+git clone https://github.com/WoolenWang/vim.git
+move /y %USERPROFILE%\.vim %USERPROFILE%\.vim_old 
+move /y %USERPROFILE%\vim %USERPROFILE%\.vim 
+move /y %USERPROFILE%\.vim\_vimrc %USERPROFILE%\ 
+git clone http://github.com/gmarik/vundle.git %USERPROFILE%\.vim\bundle\vundle
+echo "woolenÕýÔÚÅ¬Á¦ÎªÄú°²×°bundle³ÌÐò" > woolen
+echo "°²×°Íê±Ï½«×Ô¶¯ÍË³ö" >> woolen
+echo "ÇëÄÍÐÄµÈ´ý" >> woolen
 vim woolen -c "BundleInstall" -c "q" -c "q"
 rm woolen
