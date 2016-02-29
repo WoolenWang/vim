8a2e7d90408f96174f7d68e7b4d419c45f524629
WoolenWang <349526042@qq.com>
2016-03-01 00:09:00 +0800

    Update setup.sh
    https

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ copy_files_to_home()
 }
 make_all_bundle_plugin()
 {
-    git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
     echo "woolen正在努力为您安装bundle程序" > woolen
     echo "安装完毕将自动退出" >> woolen
     echo "请耐心等待" >> woolen
